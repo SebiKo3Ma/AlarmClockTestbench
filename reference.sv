dd72a52b01af94edadd1eb3fc62b54eee5ecd54b
Sebi Cozma <sebi.sam.cozma@gmail.com>
2025-01-09 18:29:11 +0200

    finished scoreboard

--- reference.sv
+++ reference.sv
@@ -4,7 +4,14 @@ class reference;
    logic [3:0] last_M1;
    logic [3:0] last_M0;
    logic [3:0] last_S1;
-   logic [3:0] last_S0; 
+   logic [3:0] last_S0;
+
+   logic [1:0] alarm_H1;
+   logic [3:0] alarm_H0;
+   logic [3:0] alarm_M1;
+   logic [3:0] alarm_M0; 
+
+   bit al_sound;
   
   function new();
     last_H1 = 2'b00;
@@ -13,6 +20,13 @@ class reference;
     last_M0 = 4'b0000;
     last_S1 = 4'b0000;
     last_S0 = 4'b0000;
+
+    alarm_H1 = 2'b00;
+    alarm_H0 = 4'b0000;
+    alarm_M1 = 4'b0000;
+    alarm_M0 = 4'b0000;
+
+    al_sound = 1'b0;
   endfunction
   
   function transaction process(transaction trans);
@@ -20,7 +34,7 @@ class reference;
     
     ref_trans = new();
     
-    if(trans.reset != 1'b0) begin
+    if(trans.reset) begin
         last_H1 = trans.H_in1;
         last_H0 = trans.H_in0;
         last_M1 = trans.M_in1;
@@ -28,13 +42,29 @@ class reference;
         last_S1 = 4'b0000;
         last_S0 = 4'b0000;
 
+        alarm_H1 = 2'b00;
+        alarm_H0 = 4'b0000;
+        alarm_M1 = 4'b0000;
+        alarm_M0 = 4'b0000;
+
+        al_sound = 1'b0;
+
         ref_trans.H_out1 = trans.H_in1;
         ref_trans.H_out0 = trans.H_in0;
         ref_trans.M_out1 = trans.M_in1;
         ref_trans.M_out0 = trans.M_in0;
         ref_trans.S_out1 = 4'b0000;
         ref_trans.S_out0 = 4'b0000;
+        ref_trans.Alarm = 1'b0;
     end else begin
+      if(alarm_H1 == last_H1 && alarm_H0 == last_H0 && alarm_M1 == last_M1 && alarm_M0 == last_M0) begin
+        if(trans.AL_ON) begin
+          al_sound = 1'b1;
+        end else begin
+          al_sound = 1'b0;
+        end
+      end
+
       if(trans.LD_time) begin
         last_H1 = trans.H_in1;
         last_H0 = trans.H_in0;
@@ -82,6 +112,23 @@ class reference;
         ref_trans.S_out0 = last_S0;
       end
       
+      if(trans.LD_alarm) begin
+        alarm_H1 = trans.H_in1;
+        alarm_H0 = trans.H_in0;
+        alarm_M1 = trans.M_in1;
+        alarm_M0 = trans.M_in0;
+      end
+
+      if(trans.STOP_al) begin
+        al_sound = 1'b0;
+      end
+
+      if(al_sound) begin
+        ref_trans.Alarm = 1'b1;
+      end else begin
+        ref_trans.Alarm = 1'b0;
+      end
+
     end
     
     ref_trans.reset = trans.reset;
@@ -95,9 +142,8 @@ class reference;
     ref_trans.LD_alarm = trans.LD_alarm;
     ref_trans.STOP_al = trans.STOP_al;
     ref_trans.AL_ON = trans.AL_ON;
-
-    ref_trans.Alarm = trans.Alarm;
     
+    $display("\nAlarm: %1d%1d:%1d%1d, Sound: %1d", alarm_H1, alarm_H0, alarm_M1, alarm_M0, al_sound);
     return ref_trans;
   endfunction
 endclass

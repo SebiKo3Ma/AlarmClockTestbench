b8d5d57f7db28f72bf2cbc9179947d6cf66ba9aa
Sebi Cozma <sebi.sam.cozma@gmail.com>
2025-01-13 16:49:51 +0200

    fixed reference reset bug

--- reference.sv
+++ reference.sv
@@ -12,6 +12,7 @@ class reference;
    logic [3:0] alarm_M0; 
 
    bit al_sound;
+   bit rst;
   
   function new();
     last_H1 = 2'b00;
@@ -27,6 +28,8 @@ class reference;
     alarm_M0 = 4'b0000;
 
     al_sound = 1'b0;
+
+    rst = 1'b0;
   endfunction
   
   function transaction process(transaction trans);
@@ -35,10 +38,13 @@ class reference;
     ref_trans = new();
     
     if(trans.reset) begin
+      if(!rst) begin
+        rst = 1'b1;
         last_H1 = trans.H_in1;
         last_H0 = trans.H_in0;
         last_M1 = trans.M_in1;
         last_M0 = trans.M_in0;
+      end
         last_S1 = 4'b0000;
         last_S0 = 4'b0000;
 
@@ -49,14 +55,15 @@ class reference;
 
         al_sound = 1'b0;
 
-        ref_trans.H_out1 = trans.H_in1;
-        ref_trans.H_out0 = trans.H_in0;
-        ref_trans.M_out1 = trans.M_in1;
-        ref_trans.M_out0 = trans.M_in0;
+        ref_trans.H_out1 = last_H1;
+        ref_trans.H_out0 = last_H0;
+        ref_trans.M_out1 = last_M1;
+        ref_trans.M_out0 = last_M0;
         ref_trans.S_out1 = 4'b0000;
         ref_trans.S_out0 = 4'b0000;
         ref_trans.Alarm = 1'b0;
     end else begin
+      rst = 1'b0;
       if(alarm_H1 == last_H1 && alarm_H0 == last_H0 && alarm_M1 == last_M1 && alarm_M0 == last_M0) begin
         if(trans.AL_ON) begin
           al_sound = 1'b1;
@@ -143,7 +150,7 @@ class reference;
     ref_trans.STOP_al = trans.STOP_al;
     ref_trans.AL_ON = trans.AL_ON;
     
-    $display("\nAlarm: %1d%1d:%1d%1d, Sound: %1d", alarm_H1, alarm_H0, alarm_M1, alarm_M0, al_sound);
+    $display("\nLast_time: %1d%1d:%1d%1d:%1d%1d, Alarm: %1d%1d:%1d%1d, Sound: %1d", last_H1, last_H0, last_M1, last_M0, last_S1, last_S0, alarm_H1, alarm_H0, alarm_M1, alarm_M0, al_sound);
     return ref_trans;
   endfunction
 endclass
